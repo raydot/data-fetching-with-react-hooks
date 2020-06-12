473d6f530a6686354e535e8ad2313229194d6000
David Kanter <david.kanter@herodigital.com>
2020-06-11 20:43:44 -0700

    Got a basic data retrieval working.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Show a list of Hacker News Articles
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  // State and update state set to an empty list of objects.
+  const [data, setData] = useState({ hits: [] });
+
+  // Use Axios to fetch data
+  // This is a little different than it is in the tutorial.  When I did it
+  // that way I got a very specific error message telling me to do it this way.
+  useEffect(() => {
+    // Run when the component mounts.
+    async function fetchData() {
+      const result = await axios(
+        'https://hn.algolia.com/api/v1/search?query=redux'
+      );
+      console.log(result);
+      setData(result.data);
+    }
+    fetchData();
+  }, []); // add [] to only fetch data when the component mounts
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {data.hits.map((item) => (
+        <li key={item.objectID}>
+          <a href={item.url}>{item.title}</a>
+        </li>
+      ))}
+    </ul>
   );
 }
 
